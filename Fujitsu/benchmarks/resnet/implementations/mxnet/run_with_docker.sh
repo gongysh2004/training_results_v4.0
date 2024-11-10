df40990840367a9c0dcc24a0cde7b62e480dee05
yong sheng gong <gong.yongsheng@99cloud.net>
2024-11-10 14:32:15 +0800

    test resnet on jy cetern

--- Fujitsu/benchmarks/resnet/implementations/mxnet/run_with_docker.sh
+++ Fujitsu/benchmarks/resnet/implementations/mxnet/run_with_docker.sh
@@ -70,7 +70,7 @@ cleanup_docker
 trap 'set -eux; cleanup_docker' EXIT
 
 # Setup container
-nvidia-docker run --rm --init --detach \
+docker run -d \
     --net=host --uts=host --ipc=host --security-opt=seccomp=unconfined \
     --ulimit=stack=67108864 --ulimit=memlock=-1 \
     --name="${_cont_name}" "${_cont_mounts[@]}" \
